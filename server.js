33640535faddbc26474af6c25b2f18220fb657f1
Michael Campos <camposmj89@gmail.com>
2016-03-05 14:15:40 -0800

    Updates to the server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var CMUDict = require('cmudict').CMUDict;
 var cmudict = new CMUDict();
-var phoneme_str = cmudict.get('prosaic');
+var phoneme_str;
 
-// var res = phoneme_str.split(" ");
 
-// for(var i = 0; i < res.length; i++) {
-// 	if (res[i].length > 1) {
-// 		var temp = res[i].split("");
-// 		console.log(temp);
-// 	};
-// }
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+	console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+	process.exit(1);
+}
 
+// Read the file and print its contents.
+var fs = require('fs');
+var filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', function(err, data) {
+	if (err) throw err;
+
+	var messageArray = data.split(" ");
+
+	for(var i = 0; i < messageArray.length; i++) {
+		phoneme_str = cmudict.get(messageArray[i]);
+		console.log(messageArray[i] + ": " + phoneme_str)
+	}
+});
